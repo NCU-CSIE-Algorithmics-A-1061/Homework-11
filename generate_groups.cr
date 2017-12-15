e5bc3132e03b5dd45b062490e2ca813296b01ad5
Tatsujin Chin <c910335@gmail.com>
2017-12-15 22:52:45 +0800

    generate groups by travis ci

---
+++ generate_groups.cr
@@ -0,0 +1,15 @@
+probabilities = [0, 2, 2, 2, 0, 3, 2, 2, 3, 0, 0, 0, 3, 2, 0, 0, 3, 0, 0, 2]
+draw_box = [] of Int32
+winners = [] of Int32
+
+probabilities.each_with_index do |p, i|
+  draw_box += [i + 1] * p
+end
+
+7.times do
+  winner = draw_box.sample
+  draw_box.delete winner
+  winners << winner
+end
+
+p winners.sort!
